8782b5cc18fe4c5d610520546c768ac2ecb3385b
UchihaCoder <137582039+3x6mr@users.noreply.github.com>
2024-01-12 17:49:33 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef();
 
+  const addTask = () => {
+    if (inputRef.current.value) {
+      const text = inputRef.current.value;
+      const newItem = { completed: false, text };
+      setTodos([...todos, newItem]);
+      inputRef.current.value = "";
+    }
+  };
+  const deleteTask = (index) => {
+    const newList = [...todos];
+    newList.splice(index, 1);
+    setTodos(newList);
+  };
+  const handleComplete = (index) => {
+    const newList = [...todos];
+    newList[index].completed = !newList[index].completed;
+    setTodos(newList);
+  };
+  {
+    return (
+      <div className="App">
+        <div className="h2-container">
+          <h2>To Do List</h2>
+        </div>
+        <div className="todos">
+          <ul>
+            {todos.map((item, index) => {
+              return (
+                <div className="task">
+                  <li
+                    key={index}
+                    className={item.completed ? "done" : ""}
+                    onClick={() => handleComplete(index)}
+                  >
+                    {item.text}
+                  </li>
+                  <span onClick={() => deleteTask(index)}>❌</span>
+                </div>
+              );
+            })}
+          </ul>
+          <input
+            className="input"
+            ref={inputRef}
+            placeholder="Enter Mission...."
+          />
+          <button className="btn" onClick={addTask}>
+            Add
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
